6dec9690ec3a0f5744e4380fcea85402e999b8d2
ricardo_grocha <1100662@isep.ipp.pt>
2020-11-17 19:27:24 +0000

    [ExpressJS] : Basic Routing

--- express-js/app.js
+++ express-js/app.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const app = express();
 
-app.use((request, response, next) => {
-    console.log('In the middleware');
+app.use('/', (request, response, next) => {
+    console.log('This always run!');
     next();
 });
 
-app.use((request, response, next) => {
-    console.log('Another middleware');
-    response.send('<h1>Hello from express.js</h1>');
+app.use('/add-product', (request, response, next) => {
+    console.log('In the middleware');
+    response.send('<h1>The "add product" page!');
 });
 
+app.use('/', (request, response, next) => {
+    console.log('In the middleware');
+    response.send('<h1>Hello from Express!');
+});
 app.listen(3000);
