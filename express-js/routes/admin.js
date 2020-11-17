b5d70b317305115a40bafc3fbf537f73eca085a1
ricardo_grocha <1100662@isep.ipp.pt>
2020-11-17 23:02:28 +0000

    [ExpressJS] Using navigation helper function

--- express-js/routes/admin.js
+++ express-js/routes/admin.js
@@ -2,9 +2,11 @@ const express = require('express');
 const path = require('path');
 const router = express.Router();
 
+const rootDir = require('../util/path');
+
 // /admin/add-product => GET
 router.get('/add-product', (request, response) => {
-   response.sendFile(path.join(__dirname,'../', 'views', 'add-product.html'))
+   response.sendFile(path.join(rootDir, 'views', 'add-product.html'))
 });
 
 // /admin/add-product => POST
--- express-js/routes/shop.js
+++ express-js/routes/shop.js
@@ -2,8 +2,10 @@ const express = require('express');
 const path = require('path');
 const router = express.Router();
 
+const rootDir = require('../util/path');
+
 router.get('/', (request, response) => {
-    response.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
+    response.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
 module.exports = router;
