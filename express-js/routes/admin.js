c70cc2ffe24a69e485ae6a6c3be09d63b4175007
ricardo_grocha <1100662@isep.ipp.pt>
2020-11-17 22:26:45 +0000

    [ExpressJS]: Filtering Paths

--- express-js/routes/admin.js
+++ express-js/routes/admin.js
@@ -2,11 +2,13 @@ const express = require('express');
 
 const router = express.Router();
 
+// /admin/add-product => GET
 router.get('/add-product', (request, response) => {
-    response.send(`<form action="/product" method="POST"><input type="text" name="title" /><button type="submit">Submit</button></form>`);
+    response.send(`<form action="/admin/add-product" method="POST"><input type="text" name="title" /><button type="submit">Submit</button></form>`);
 });
 
-router.post('/product', (request, response) => {
+// /admin/add-product => POST
+router.post('/add-product', (request, response) => {
     console.log(request.body);
     response.redirect('/')
 });
