840efa5e6b87a2876eede1139b3650c3107c3afc
RicardoGomesRocha <ricardo1992rocha@gmail.com>
2021-04-22 14:08:46 -0400

    adding the Signin Feature

--- express-js/controllers/auth.js
+++ express-js/controllers/auth.js
@@ -1,39 +1,62 @@
 const User = require('../models/user');
+const bcrypt = require('bcryptjs');
 
 exports.getLogin = (req, res, next) => {
-  res.render('auth/login', {
-    path: '/login',
-    pageTitle: 'Login',
-    isAuthenticated: false
-  });
+    res.render('auth/login', {
+        path: '/login',
+        pageTitle: 'Login',
+        isAuthenticated: false
+    });
 };
 
 exports.getSignup = (req, res, next) => {
-  res.render('auth/signup', {
-    path: '/signup',
-    pageTitle: 'Signup',
-    isAuthenticated: false
-  });
+    res.render('auth/signup', {
+        path: '/signup',
+        pageTitle: 'Signup',
+        isAuthenticated: false
+    });
 };
 
-exports.postLogin = (req, res, next) => {
-  User.findById('5bab316ce0a7c75f783cb8a8')
-    .then(user => {
-      req.session.isLoggedIn = true;
-      req.session.user = user;
-      req.session.save(err => {
-        console.log(err);
+exports.postLogin = async (req, res, next) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    const user = await User.findOne({
+        email
+    });
+    if (!user) return res.redirect('/login');
+
+    const passwordsMatch = await bcrypt.compare(password, user.password);
+    if (passwordsMatch) {
+        req.session.isLoggedIn = true;
+        req.session.user = user;
+        await req.session.save();
         res.redirect('/');
-      });
-    })
-    .catch(err => console.log(err));
+    } else {
+        return res.redirect('/login')
+    }
 };
 
-exports.postSignup = (req, res, next) => {};
+exports.postSignup = async (req, res, next) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    const confirmPassword = req.body.confirmPassword;
+
+    let user = await User.findOne({
+        email
+    });
+    if (user) return res.redirect('/signup');
+    user = new User({
+        name: email,
+        email,
+        password: await bcrypt.hash(password, 12)
+    });
+    await user.save();
+    res.redirect('/login');
+};
 
 exports.postLogout = (req, res, next) => {
-  req.session.destroy(err => {
-    console.log(err);
-    res.redirect('/');
-  });
+    req.session.destroy(err => {
+        console.log(err);
+        res.redirect('/');
+    });
 };
