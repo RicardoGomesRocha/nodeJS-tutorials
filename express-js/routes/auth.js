75cc4f13652fdf612ea4cb7e9ae1bdca1dce315a
RicardoGomesRocha <ricardo1992rocha@gmail.com>
2021-04-23 15:43:03 -0400

    Adding auhtorization and password reset.

--- express-js/routes/auth.js
+++ express-js/routes/auth.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { check } = require('express-validator/check');
 
 const authController = require('../controllers/auth');
 
@@ -10,8 +11,16 @@ router.get('/signup', authController.getSignup);
 
 router.post('/login', authController.postLogin);
 
-router.post('/signup', authController.postSignup);
+router.post('/signup', check('email').isEmail(), authController.postSignup);
 
 router.post('/logout', authController.postLogout);
+
+router.get('/reset', authController.getReset);
+
+router.post('/reset', authController.postReset);
+
+router.get('/reset/:token', authController.getNewPassword);
+
+router.post('/new-password', authController.postNewPassword);
 
 module.exports = router;
